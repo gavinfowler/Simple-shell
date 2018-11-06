98faa47870d9cbc9e8c549f6c8932b12969d1760
nightblood <gavins.username@gmail.com>
2018-11-06 16:23:55 -0700

    WIP n pipes

--- main.cpp
+++ main.cpp
@@ -214,6 +214,57 @@ char *convert(const std::string & s){
 	return pc; 
 }
 
+struct command{
+	const char **argv;
+	command& operator=(std::vector<char*> a){
+		argv = const_cast<const char**>(&a[0]);
+	}
+
+};
+
+int spawn_proc (int in, int out, command *cmd){
+	pid_t pid;
+	if ((pid = fork ()) == 0){
+		if (in != 0){
+			dup2 (in, 0);
+			close (in);
+		}
+	        if (out != 1){
+			dup2 (out, 1);
+			close (out);
+		}
+	        return execvp (cmd->argv [0], (char * const *)cmd->argv);
+        }
+	return pid;
+}
+
+int
+fork_pipes (int n, struct command *cmd)
+{
+	int i;
+	pid_t pid;
+	int in, fd [2];
+        /* The first process should get its input from the original file descriptor 0.  */
+        in = 0;
+        /* Note the loop bound, we spawn here all, but the last stage of the pipeline.  */
+      	for (i = 0; i < n - 1; ++i){
+		pipe (fd);
+                /* f [1] is the write end of the pipe, we carry `in` from the prev iteration.  */
+                spawn_proc (in, fd [1], cmd + i);
+ 	        /* No need for the write end of the pipe, the child will write here.  */
+	        close (fd [1]);
+                /* Keep the read end of the pipe, the next child will read from there.  */
+	        in = fd [0];
+	}
+        /* Last stage of the pipeline - set stdin be the read end of the previous pipe
+	 *      and output to the original file descriptor 1. */  
+	if (in != 0)
+		dup2 (in, 0);
+	/* Execute the last stage with the current process. */
+	return execvp (cmd [i].argv [0], (char * const *)cmd [i].argv);
+}
+
+
 void execute(char **argv, std::vector<std::string> arguments){
 	pid_t pid;
 	int status;
@@ -237,7 +288,30 @@ void execute(char **argv, std::vector<std::string> arguments){
 		}
 	}
 	else{
+		std::vector<std::vector<char*>> cmd;
+		std::cout << sizeof(*argv)/sizeof(**argv) <<std::endl;
+		//struct command cmd [arguments.size()+1];
 		std::vector<char*> first1;
+		int i = 0;
+		std::cout << "prewhileloop\n";
+		while(arguments.size()>0){
+			std::transform(arguments.begin(), std::find(arguments.begin(),
+				arguments.end(), "|"), std::back_inserter(first1), convert);
+			std::cout << arguments.size() <<std::endl;
+			cmd.push_back(first1);
+			std::cout << i <<std::endl;
+			i++;
+			if (std::distance(arguments.begin(), arguments.erase(arguments.begin(), 
+				std::find(arguments.begin(), arguments.end(), "|"))) == 0)
+				break;
+			std::cout << first1.size() <<std::endl;
+		}
+		std::cout << "prepiping\n";
+		//int k = fork_pipes(3, cmd);
+		return;
+		
+
+		//std::vector<char*> first1;
 		std::vector<char*> second1;
 		std::transform(arguments.begin(), std::find(arguments.begin(), arguments.end(), "|"),
 			       	std::back_inserter(first1), convert);
